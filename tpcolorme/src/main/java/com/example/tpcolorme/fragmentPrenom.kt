2f22612c34a27e3122583ef5f0ec8fad6732c202
Yannick Biheul <yannick.biheul2022@campus-eni.fr>
2022-07-07 10:10:39 +0200

    TP Color Me

---
+++ tpcolorme/src/main/java/com/example/tpcolorme/fragmentPrenom.kt
@@ -0,0 +1,36 @@
+package com.example.tpcolorme
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.Button
+import androidx.fragment.app.Fragment
+import androidx.navigation.Navigation
+import kotlin.random.Random
+
+class fragmentPrenom : Fragment() {
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+
+        return inflater.inflate(R.layout.fragment_prenom, container, false)
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        val button = view.findViewById<Button>(R.id.buttonPrenom)
+        // Méthode 1 (fonctionne pour tout)
+        button.setOnClickListener {
+            var random = Random.nextInt(1, 100)
+            if (random > 50) {
+                Navigation.findNavController(view).navigate(R.id.action_fragmentPrenom_to_fragmentRouge)
+            } else {
+                Navigation.findNavController(view).navigate(R.id.action_fragmentPrenom_to_fragmentRose)
+            }
+        }
+    }
+
+}
---
+++ tpcolorme/src/main/java/com/example/tpcolorme/fragmentRose.kt
@@ -0,0 +1,36 @@
+package com.example.tpcolorme
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.Button
+import androidx.fragment.app.Fragment
+import androidx.navigation.Navigation
+import kotlin.random.Random
+
+class fragmentRose : Fragment() {
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_rose, container, false)
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        val button = view.findViewById<Button>(R.id.buttonRose)
+        // Méthode 1 (fonctionne pour tout)
+        button.setOnClickListener {
+            var random = Random.nextInt(1, 100)
+            if (random > 50) {
+                Navigation.findNavController(view).navigate(R.id.action_fragmentRose_to_fragmentJaune)
+            } else {
+                Navigation.findNavController(view).navigate(R.id.action_fragmentRose_to_fragmentVert)
+            }
+        }
+    }
+
+}
